8d44ea36e6a4f50790e4cb0e5a7706da95daa9ef
arminhus <husanovicarmin@gmail.com>
2019-01-20 21:40:18 -0500

    removed getJSON for Fetch

--- App.js
+++ App.js
@@ -6,38 +6,77 @@
 const OPEN_WEATHER_KEY = '&appid=27c261aa5ec6c6dc1ab38d473048b64d';
 const OPEN_WEATHER_DAILY_LINK = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const OPEN_WEATHER_FORCAST_LINK = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 let getCurrentData = (zipCode, tempType) => {
-    $.getJSON(OPEN_WEATHER_DAILY_LINK + zipCode + OPEN_WEATHER_KEY + tempType, data => {
-        console.log('DATA' + data);
-        displayResults(data);
-        getForcastData(zipCode, tempType, data);
-    });
+    // $.getJSON(OPEN_WEATHER_DAILY_LINK + zipCode + OPEN_WEATHER_KEY + tempType, data => {
+    //     console.log('DATA' + data);
+    //     displayResults(data);
+    //     getForcastData(zipCode, tempType, data);
+    // });
+    console.log(OPEN_WEATHER_DAILY_LINK + zipCode + OPEN_WEATHER_KEY + tempType);
+    fetch(OPEN_WEATHER_DAILY_LINK + zipCode + OPEN_WEATHER_KEY + tempType)
+        .then(res => res.json())
+        .then(dailyTemp => $("#temp").html(`
+    
+         <div id="js-result">
+                 <p>Current</p>
+                 <p id='temp'>${dailyTemp.main.temp}°</p>
+                 <p>Upcoming Week</p>
+                 <div id='forecast'></div>
+             </div>
+         `));
 };
 
 let getForcastData = (zipCode, tempType, data) => {
-    $.getJSON(OPEN_WEATHER_FORCAST_LINK + zipCode + OPEN_WEATHER_KEY + tempType, fData => {
-        // console.log(fData);
-        displayForcastResults(fData, data);
-    });
+    // $.getJSON(OPEN_WEATHER_FORCAST_LINK + zipCode + OPEN_WEATHER_KEY + tempType, fData => {
+    //     // console.log(fData);
+    //     displayForcastResults(fData, data);
+    // });
+    fetch(OPEN_WEATHER_FORCAST_LINK + zipCode + OPEN_WEATHER_KEY + tempType)
+        .then(res => res.json())
+        .then(forcastTemp => {
+
+            return forcastTemp.list.map(item => {
+                let d = new Date(item.dt_txt)
+                if (d.getHours() === 12) {
+                    // console.log(item.dt_txt, item.main.temp);
+                    $("#forecast").append(`
+            <div class='forecast-wrap'>
+            <p class='forecast-date'>${days[d.getDay()]}</p>
+            <p class='forecast-temp'>${item.main.temp}°</p>
+            </div>
+            `);
+                }
+            });
+        });
 };
 
-let displayResults = (data) => {
-    $("#temp").html(data.main.temp);
+let displayResults = (dailyTemp) => {
+    console.log(dailyTemp.main.temp);
+    $("#temp").html(dailyTemp.main.temp);
+    // return dailyTemp.main.temp;
+
 };
 
-let displayForcastResults = (fData, data) => {
+let displayForcastResults = (forcastTemp) => {
+    console.log("HERE IS THE FORCASTTEMP" + forcastTemp);
+    forcastTemp.list.map(item => {
+        let d = new Date()
+        let d2 = new Date(item.dt_txt)
+        console.log(d.getDay(), d2)
+    })
     $('#result-main').html(`
     
     <div id="js-result">
             <p>Current</p>
-            <p id='temp'>${data.main.temp}°</p>
+            <p id='temp'>${'hi'}°</p>
             <p>Upcoming Week</p>
             <div id='forecast'></div>
         </div>
     `)
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return fData.list.map(item => {
+
+    return forcastTemp.list.map(item => {
         let d = new Date(item.dt_txt)
         if (d.getHours() === 12) {
             // console.log(item.dt_txt, item.main.temp);
@@ -58,7 +97,7 @@ let submitEvent = () => {
         let tempType = $("input[name=weather-type]:checked").val();
         tempType = '&units=' + tempType;
         getCurrentData(zipCode, tempType);
-        // getForcastData(zipCode, tempType);
+        getForcastData(zipCode, tempType);
     });
 };
 
